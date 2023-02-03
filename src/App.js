de9252b739e7b0e84ed5e864621ea10c2967492c
CGH <tt878429@gmail.com>
2023-02-03 23:44:54 +0900

    Tes and code button click

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [ buttonColor, setButtonColor ] = useState('red');
-  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
   return (
     <div >
       <button 
-        style={{backgroundColor: buttonColor}}
-        onClick={() => setButtonColor(newButtonColor)}>
+        style={{ backgroundColor: buttonColor }} 
+        onClick={() => setButtonColor(newButtonColor)} 
+      >
         Change to {newButtonColor}
       </button>
     </div>
