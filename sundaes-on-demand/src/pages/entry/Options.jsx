7400cd6a7a82d0d4bbabff062fe3c5d9f572e44e
CGH <tt878429@gmail.com>
2023-02-05 20:08:15 +0900

    findAllByRole  with msw

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,38 +1,32 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Row } from 'react-bootstrap';
-import ScoopOptions from './ScoopOptions';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOptions from "./ScoopOptions";
+import ToppingOptions from './ToppingOptions';
 
-const Options = ({optionType}) => {
+export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
 
-  // optionType is 'sccops' or 'toppings'
+  // optionType is 'scoops' or 'toppings
   useEffect(() => {
-    const requestData = async () => {
-      try {
-        const response = await axios.get(`http://localhost:3030/${optionType}`);
-        setItems(response.data);
-      } catch (e) {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
         // TODO: handle error response
-      }
-    }
-    requestData();
+      });
   }, [optionType]);
 
   // TODO: replace `null` with ToppingOption when available
-  const ItemComponent = optionType === "scoops" ? ScoopOptions : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
 
-  const optionItems = items.map(item => (
-    <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
-  ))
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
 
-  return (
-    <Row>
-      {
-        optionItems
-      }
-    </Row>
-  )
+  return <Row>{optionItems}</Row>;
 }
-
-export default Options;
--- sundaes-on-demand/src/pages/entry/test/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/test/Options.test.jsx
@@ -1,16 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
+import Options from "../Options";
 
-import Options from '../Options';
-import ScoopOptions from '../ScoopOptions';
-
-test('displays image for each scoop options from server', () => {
+test("displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
   // find images
-  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   // confirm alt text of images
-  const altText = scoopImages.map(element => element.alt);
-  expect(altText).toEqual(['Chocolate sccop', 'Vanilla scoop']);
+  // @ts-ignore
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("display image for each topping option form server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(toppingImages).toHaveLength(3);
+
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual(["M&Ms topping", "Hot fudge topping", "Peanut butter cups topping"]);
 })
