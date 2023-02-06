36ea94f579245e124d9d84096b85a701bf563156
CGH <tt878429@gmail.com>
2023-02-06 23:36:20 +0900

    Test and code context for scoops subtotal

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -4,10 +4,14 @@ import Row from "react-bootstrap/Row";
 import ScoopOptions from "./ScoopOptions";
 import ToppingOptions from "./ToppingOptions";
 import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constant";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../context/OrderDetails";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
@@ -24,10 +28,20 @@ export default function Options({ optionType }) {
   }
   // TODO: replace `null` with ToppingOption when available
   const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each </p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
